a539c4d4a5e7d5b87b7a21bbb78b865b93ffa5b8
Jobsfotogrid <59364674+Jobsfotogrid@users.noreply.github.com>
2021-02-13 22:03:19 -0300

    Exclusão da classe principal escrita em Kotlin
    Após a exclusão da classe principal foi criada uma nova classe principal escrita em java para executar o programa.

---
+++ app/src/main/java/br/com/jobsfotogrid/googleplay/Main.java
@@ -0,0 +1,13 @@
+package br.com.jobsfotogrid.googleplay;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
